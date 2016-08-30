cb50e94038f7e5cb6231b8498ca80cdba6179ef4
ALong <zengweilong@vipkid.com.cn>
2016-08-30 17:22:01 +0800

    防止老师多次点击后出现异常

--- trpm-dao/src/main/java/com/vipkid/trpm/dao/TeacherPageLoginDao.java
+++ trpm-dao/src/main/java/com/vipkid/trpm/dao/TeacherPageLoginDao.java
@@ -1,7 +1,12 @@
 package com.vipkid.trpm.dao;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.community.dao.support.MapperDaoTemplate;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +15,8 @@ import com.vipkid.trpm.entity.TeacherPageLogin;
 @Repository
 public class TeacherPageLoginDao extends MapperDaoTemplate<TeacherPageLogin> {
 
+    private static Logger logger = LoggerFactory.getLogger(TeacherPageLoginDao.class);
+    
 	@Autowired
 	public TeacherPageLoginDao(SqlSessionTemplate sqlSessionTemplate) {
 		super(sqlSessionTemplate, TeacherPageLogin.class);
@@ -24,7 +31,12 @@ public class TeacherPageLoginDao extends MapperDaoTemplate<TeacherPageLogin> {
     }
 
 	public int saveTeacherPageLogin(TeacherPageLogin teacherPageLogin) {
-		return super.save(teacherPageLogin);
+	    List<TeacherPageLogin> list = this.selectList(teacherPageLogin);
+	    if(CollectionUtils.isNotEmpty(list)){
+	        logger.warn("老师:{},已经点击过loginType:{}",teacherPageLogin.getUserId(),teacherPageLogin.getLoginType());
+	        return 1;
+	    }
+	    return super.save(teacherPageLogin);
 	}
 
 }
