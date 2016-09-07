868c648d1052a2ca0edd91fb9cc7cc22bc505825
zouqinghua <zouqinghua@vipkid.com.cn>
2016-09-07 17:32:58 +0800

    增加重置密码发邮件api接口

---
+++ trpm-web/src/main/java/com/vipkid/rest/TeacherRestController.java
@@ -0,0 +1,47 @@
+package com.vipkid.rest;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Maps;
+import com.vipkid.http.utils.JsonUtils;
+import com.vipkid.trpm.entity.Teacher;
+import com.vipkid.trpm.entity.TeacherAddress;
+import com.vipkid.trpm.entity.TeacherLocation;
+import com.vipkid.trpm.entity.User;
+import com.vipkid.trpm.service.passport.PassportService;
+import com.vipkid.trpm.service.portal.PersonalInfoService;
+import com.vipkid.trpm.service.portal.TeacherService;
+
+
+@RestController
+public class TeacherRestController {
+
+	private Logger logger = LoggerFactory.getLogger(TeacherRestController.class);
+	
+	@Resource
+	private PassportService passportService;
+	
+	@RequestMapping(value = "/teacherPassword/sendEmail")
+	public Map<String, String> getBankInfoByTeacherId(Long userId,HttpServletRequest request, HttpServletResponse response) {
+		logger.info(" 重置教师密码  teacherId = {}",userId);
+		Map<String, String> resultMap = null;
+		if(userId!=null){
+			User user = new User();
+			user.setId(userId);
+			resultMap = passportService.senEmailForPassword(user);
+		}
+		logger.info("教师银行信息 userId = {} , resultMap = {}",userId,JsonUtils.toJSONString(resultMap));
+		
+		return resultMap;
+	}
+
+}
