0e4bbb8b0b95d8a7a3fb6f9361d8398554d1f600
ALong <zengweilong@vipkid.com.cn>
2016-10-11 17:14:29 +0800

    sendto

--- trpm-web/src/main/java/com/vipkid/email/handle/EmailServer.java
+++ trpm-web/src/main/java/com/vipkid/email/handle/EmailServer.java
@@ -15,12 +15,44 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.internet.MimeUtility;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EmailServer {
     
     private static Logger logger = LoggerFactory.getLogger(EmailServer.class);
+    
+    private static int sendMessage(String from,Session _session,EmailEntity emailEntity){
+        try {
+            String toMail = emailEntity.getToMail(); // 邮件接收者的地址
+            String title = emailEntity.getMailSubject(); // 邮件主题
+            final String content = emailEntity.getMailBody();//邮件内容
+            String personalName = emailEntity.getPersonalName(); // 我的称乎
+            if(StringUtils.isNotBlank(personalName)){
+                personalName = from.split("@")[0];
+            }
+            logger.info("Email send start ====>：");
+            MimeMessage message = new MimeMessage(_session);            
+            MimeMultipart mimeMultipart = new MimeMultipart();
+            MimeBodyPart bodyPart = new MimeBodyPart();
+            bodyPart.setContent(content, "text/html; charset=utf-8");// 设置邮件格式
+            //bodyPart.addHeader("Content-Transfer-Encoding", "base64");// 设置邮件标题
+            mimeMultipart.addBodyPart(bodyPart);
+            message.setContent(mimeMultipart);
+            message.setSubject(MimeUtility.encodeText(title, StandardCharsets.UTF_8.toString(), "B")); // 设置邮件主题
+            message.setSentDate(new Date()); // 设置邮件发送时期
+            message.setFrom(new InternetAddress(from, personalName)); // 设置邮件发送者的地址
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(toMail)); // 设置邮件接收者的地址
+            logger.info("Email send ing ====>："+toMail+"_"+title);
+            Transport.send(message);
+        } catch (Exception e) {
+            logger.error("Emial send Exceprion:", e);
+            return 0;
+        }
+        logger.info("Email send complete ====>：");
+        return 1;
+    }
     
     static class Teachvip{ 
     
@@ -41,39 +73,9 @@ public class EmailServer {
             });
         }
         
-        /**
-         * 邮件发送
-         * @param reviceEntity 发送信息对象
-         * @param authenticator 登录信息对象
-         * @return
-         * @throws javax.mail.SendFailedException
-         */
         public static int sendMail(EmailEntity emailEntity){
-          try {
-              String toMail = emailEntity.getToMail(); // 邮件接收者的地址
-              String title = emailEntity.getMailSubject(); // 邮件主题
-              final String content = emailEntity.getMailBody();//邮件内容
-              String personalName = emailEntity.getPersonalName(); // 我的称乎
-              logger.info("Email send start ====>：");
-              MimeMessage message = new MimeMessage(_session);            
-              MimeMultipart mimeMultipart = new MimeMultipart();
-              MimeBodyPart bodyPart = new MimeBodyPart();
-              bodyPart.setContent(content, "text/html; charset=utf-8");// 设置邮件格式
-              mimeMultipart.addBodyPart(bodyPart);
-              message.setContent(mimeMultipart);
-              message.setSubject(MimeUtility.encodeText(title, StandardCharsets.UTF_8.toString(), "B")); // 设置邮件主题
-              message.setSentDate(new Date()); // 设置邮件发送时期
-              message.setFrom(new InternetAddress(emailEntity.getFromMail(), personalName)); // 设置邮件发送者的地址
-              message.addRecipient(Message.RecipientType.TO, new InternetAddress(toMail)); // 设置邮件接收者的地址
-              logger.info("Email send ing ====>："+toMail+"_"+title);
-              Transport.send(message);
-           } catch (Exception e) { 
-               logger.error("Emial send Exceprion:",e);
-               return 0;
-           }  
-           logger.info("Email send complete ====>：");
-           return 1;
-         } 
+            return sendMessage(EmailConfig.TC_FROM, _session, emailEntity);
+        } 
     }
     
     
@@ -96,39 +98,9 @@ public class EmailServer {
             });
         }
         
-        /**
-         * 邮件发送
-         * @param reviceEntity 发送信息对象
-         * @param authenticator 登录信息对象
-         * @return
-         * @throws javax.mail.SendFailedException
-         */
         public static int sendMail(EmailEntity emailEntity){
-          try {
-              String toMail = emailEntity.getToMail(); // 邮件接收者的地址
-              String title = emailEntity.getMailSubject(); // 邮件主题
-              final String content = emailEntity.getMailBody();//邮件内容
-              String personalName = emailEntity.getPersonalName(); // 我的称乎
-              logger.info("Email send start ====>：");
-              MimeMessage message = new MimeMessage(_session);            
-              MimeMultipart mimeMultipart = new MimeMultipart();
-              MimeBodyPart bodyPart = new MimeBodyPart();
-              bodyPart.setContent(content, "text/html; charset=utf-8");// 设置邮件格式
-              mimeMultipart.addBodyPart(bodyPart);
-              message.setContent(mimeMultipart);
-              message.setSubject(MimeUtility.encodeText(title, StandardCharsets.UTF_8.toString(), "B")); // 设置邮件主题
-              message.setSentDate(new Date()); // 设置邮件发送时期
-              message.setFrom(new InternetAddress(emailEntity.getFromMail(), personalName)); // 设置邮件发送者的地址
-              message.addRecipient(Message.RecipientType.TO, new InternetAddress(toMail)); // 设置邮件接收者的地址
-              logger.info("Email send ing ====>："+toMail+"_"+title);
-              Transport.send(message);
-           } catch (Exception e) { 
-               logger.error("Emial send Exceprion:",e);
-               return 0;
-           }  
-           logger.info("Email send complete ====>：");
-           return 1;
-         } 
+            return sendMessage(EmailConfig.ED_FROM, _session, emailEntity);
+        }
     }
     
     
