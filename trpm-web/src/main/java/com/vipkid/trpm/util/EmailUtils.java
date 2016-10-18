4bd85db465beb07e82e217023027e2890cf7f622
xingxuelin <xingxuelin@vipkid.com.cn>
2016-10-18 14:56:23 +0800

    recruitment_emails v1.3

--- trpm-web/src/main/java/com/vipkid/trpm/util/EmailUtils.java
+++ trpm-web/src/main/java/com/vipkid/trpm/util/EmailUtils.java
@@ -13,7 +13,7 @@ public class EmailUtils {
     public static void sendEmail4UndoFail(Teacher teacher) {
         Map<String, String> paramsMap = Maps.newHashMap();
         paramsMap.put("teacherName", teacher.getRealName());
-        Map<String, String> emailMap = new TempleteUtils().readTemplete("BasicInfoUndoFail.html", paramsMap, "BasicInfoUndoFailTitle.html");
+        Map<String, String> emailMap = new TempleteUtils().readTemplete("BasicInfoPass.html", paramsMap, "BasicInfoPassTitle.html");
         new EmailEngine().addMailPool(teacher.getEmail(), emailMap, EmailConfig.EmailFormEnum.TEACHVIP);
     }
 }
