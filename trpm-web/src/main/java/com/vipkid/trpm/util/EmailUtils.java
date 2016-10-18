4269d4b170b0e9213fc4158ac0c44e5644bedb6e
ALong <zengweilong@vipkid.com.cn>
2016-10-18 14:45:58 +0800

    sendMail

---
+++ trpm-web/src/main/java/com/vipkid/trpm/util/EmailUtils.java
@@ -0,0 +1,19 @@
+package com.vipkid.trpm.util;
+
+import java.util.Map;
+
+import com.google.api.client.util.Maps;
+import com.vipkid.email.EmailEngine;
+import com.vipkid.email.handle.EmailConfig;
+import com.vipkid.email.templete.TempleteUtils;
+import com.vipkid.trpm.entity.Teacher;
+
+public class EmailUtils {
+
+    public static void sendEmail4UndoFail(Teacher teacher) {
+        Map<String, String> paramsMap = Maps.newHashMap();
+        paramsMap.put("teacherName", teacher.getRealName());
+        Map<String, String> emailMap = new TempleteUtils().readTemplete("BasicInfoUndoFail.html", paramsMap, "BasicInfoUndoFailTitle.html");
+        new EmailEngine().addMailPool(teacher.getEmail(), emailMap, EmailConfig.EmailFormEnum.TEACHVIP);
+    }
+}
