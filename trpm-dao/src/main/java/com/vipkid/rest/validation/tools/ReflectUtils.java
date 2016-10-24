119d20be4243a73d42d5d333dd77970557dc1b50
ALong <zengweilong@vipkid.com.cn>
2016-10-24 13:27:29 +0800

    add reflect

--- trpm-dao/src/main/java/com/vipkid/rest/validation/tools/ReflectUtils.java
+++ trpm-dao/src/main/java/com/vipkid/rest/validation/tools/ReflectUtils.java
@@ -1,15 +1,9 @@
 package com.vipkid.rest.validation.tools;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 public class ReflectUtils {
     
@@ -29,47 +23,5 @@ public class ReflectUtils {
                logger.error(e.getMessage(),e);    
                return null;    
            }    
-       }   
-         
-    /** 
-    * 获取属性名数组 
-    * */  
-    public static String[] getFiledName(Object o){  
-        Field[] fields=o.getClass().getDeclaredFields();  
-        String[] fieldNames=new String[fields.length];  
-        for(int i=0;i<fields.length;i++){   
-            fieldNames[i]=fields[i].getName();  
-        }  
-        return fieldNames;  
-   }  
-         
-    /** 
-    * 获取属性类型(type)，属性名(name)，属性值(value)的map组成的list 
-    * 
-    * */  
-    public static List<Map<String,Object>> getFiledsInfo(Object o){  
-        Field[] fields=o.getClass().getDeclaredFields();    
-        List<Map<String,Object>> list = Lists.newArrayList();
-        Map<String,Object> infoMap=null;  
-        for(int i=0;i<fields.length;i++){  
-            infoMap = Maps.newHashMap();  
-            infoMap.put("type", fields[i].getType().toString());  
-            infoMap.put("name", fields[i].getName());  
-            infoMap.put("value", getFieldValueByName(fields[i].getName(), o));  
-            list.add(infoMap);  
-        }  
-        return list;  
-       }  
-         
-    /** 
-    * 获取对象的所有属性值，返回一个对象数组 
-    * */  
-    public static Object[] getFiledValues(Object o){  
-        String[] fieldNames = getFiledName(o);  
-        Object[] value = new Object[fieldNames.length];  
-        for(int i=0;i<fieldNames.length;i++){  
-            value[i] = getFieldValueByName(fieldNames[i], o);  
-        }
-        return value;  
-    } 
+    }   
 }
