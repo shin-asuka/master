34596f69a6cd8fef326d13ae542079b403e8bd2b
ALong <zengweilong@vipkid.com.cn>
2016-10-24 16:31:42 +0800

    最大2000

--- trpm-dao/src/main/java/com/vipkid/rest/dto/TeachingExperienceDto.java
+++ trpm-dao/src/main/java/com/vipkid/rest/dto/TeachingExperienceDto.java
@@ -20,7 +20,7 @@ public class TeachingExperienceDto{
     
     private Float hoursWeek;
     
-    @Verify(type={Type.NOT_NULL,Type.MAX_LENGTH,Type.MIN_LENGTH},maxLength=2000,minLength=100)
+    @Verify(type={Type.NOT_NULL,Type.MAX_LENGTH},maxLength=2000)
     private String jobDescription;
 
     public Long getId() {
