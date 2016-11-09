3aa4112feaaa330b896175e7fc17036b9674dd7a
liuguowen <liuguowen>
2016-11-09 14:06:53 +0800

    修改log日志输出，拦截器异常抛出

--- trpm-web/src/main/java/com/vipkid/trpm/interceptor/MonitorMethodInterceptor.java
+++ trpm-web/src/main/java/com/vipkid/trpm/interceptor/MonitorMethodInterceptor.java
@@ -8,31 +8,33 @@ import org.slf4j.LoggerFactory;
 
 public class MonitorMethodInterceptor implements MethodInterceptor {
 
-	private static final Logger logger = LoggerFactory.getLogger(MonitorMethodInterceptor.class);
+    private static final Logger logger = LoggerFactory.getLogger(MonitorMethodInterceptor.class);
 
-	/*
-	 * @param invocation
-	 * 
-	 * @return
-	 * 
-	 * @throws Throwable
-	 * 
-	 * @see
-	 * org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation
-	 * )
-	 */
-	@Override
-	public Object invoke(MethodInvocation invocation) throws Throwable {
-		long beginTime = System.nanoTime();
-		try {
-			Object result = invocation.proceed();
-			return result;
-		} finally {
-			long endTime = System.nanoTime();
-			logger.info("Method: [" + invocation.getMethod().getDeclaringClass().getName() + "."
-					+ invocation.getMethod().getName() + "()], Invocation Time: ["
-					+ MathUtils.divide((endTime - beginTime), (1000 * 1000)) + "] Millis");
-		}
-	}
+    /*
+     * @param invocation
+     * 
+     * @return
+     * 
+     * @throws Throwable
+     * 
+     * @see
+     * org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation
+     * )
+     */
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        long beginTime = System.nanoTime();
+        try {
+            Object result = invocation.proceed();
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            long endTime = System.nanoTime();
+            logger.info("Method: [" + invocation.getMethod().getDeclaringClass().getName() + "."
+                            + invocation.getMethod().getName() + "()], Invocation Time: ["
+                            + MathUtils.divide((endTime - beginTime), (1000 * 1000)) + "] Millis");
+        }
+    }
 
 }
