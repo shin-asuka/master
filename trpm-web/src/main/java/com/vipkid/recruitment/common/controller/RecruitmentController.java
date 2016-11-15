6b21ab05128b35cd8249110d47996ab82d0c1951
ALong <zengweilong@vipkid.com.cn>
2016-11-15 22:03:18 +0800

    submit~

--- trpm-web/src/main/java/com/vipkid/recruitment/common/controller/RecruitmentController.java
+++ trpm-web/src/main/java/com/vipkid/recruitment/common/controller/RecruitmentController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.common.collect.Maps;
 import com.vipkid.recruitment.common.service.RecruitmentService;
 import com.vipkid.recruitment.interceptor.RestInterface;
+import com.vipkid.recruitment.utils.ResponseUtils;
 import com.vipkid.rest.RestfulController;
 import com.vipkid.rest.config.RestfulConfig;
 import com.vipkid.rest.web.LoginController;
@@ -35,64 +36,49 @@ public class RecruitmentController extends RestfulController{
     
     @RequestMapping(value = "/getStatus", method = RequestMethod.GET, produces = RestfulConfig.JSON_UTF_8)
     public Map<String,Object> getStatus(HttpServletRequest request, HttpServletResponse response){
-        Map<String,Object> result = Maps.newHashMap();
         try{
             User user = getUser(request);
-            result =  this.recruitmentService.getStatus(user.getId());
-            return result;
+            Map<String,Object> result =  this.recruitmentService.getStatus(user.getId());
+            return ResponseUtils.responseSuccess(result);
         } catch (IllegalArgumentException e) {
-            result.put("status", false);
-            logger.error("内部参数转化异常:"+e.getMessage());
             response.setStatus(HttpStatus.BAD_REQUEST.value());
+            return ResponseUtils.responseFail(e.getMessage(), this);
         } catch (Exception e) {
-            result.put("status", false);
-            logger.error(e.getMessage(), e);
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return ResponseUtils.responseFail(e.getMessage(), this);
         }
-        return result;
     } 
     
     @RequestMapping(value = "/getTeacherInfo", method = RequestMethod.GET, produces = RestfulConfig.JSON_UTF_8)
     public Map<String,Object> getTeacherInfo(HttpServletRequest request, HttpServletResponse response){
-        Map<String,Object> result = Maps.newHashMap();
         try{
             Teacher teacher = getTeacher(request);
+            Map<String,Object> result = Maps.newHashMap();
             result.put("teacher", teacher);
             result.put("teacherAddress", this.recruitmentService.getTeacherAddress(teacher.getCurrentAddressId()));
-            result.put("status", true);
-            return result;
+            return ResponseUtils.responseSuccess(result);
         } catch (IllegalArgumentException e) {
-            result.put("status", false);
-            logger.error("内部参数转化异常:"+e.getMessage());
             response.setStatus(HttpStatus.BAD_REQUEST.value());
+            return ResponseUtils.responseFail(e.getMessage(), this);
         } catch (Exception e) {
-            result.put("status", false);
-            logger.error(e.getMessage(), e);
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return ResponseUtils.responseFail(e.getMessage(), this);
         }
-        return result;
     } 
     
     @RequestMapping(value = "/timezone", method = RequestMethod.GET, produces = RestfulConfig.JSON_UTF_8)
     public Map<String,Object> timezone(HttpServletRequest request, HttpServletResponse response,String timezone){
-        Map<String,Object> result = Maps.newHashMap();
         try{
             Teacher teacher = getTeacher(request);
             logger.info("user:{},timezone",teacher.getId());
-            boolean resultBol = this.recruitmentService.updateTimezone(timezone, teacher);
-            result.put("status", resultBol);
-            return result;
+            this.recruitmentService.updateTimezone(timezone, teacher);
+            return ResponseUtils.responseSuccess();
         } catch (IllegalArgumentException e) {
-            result.clear();
-            result.put("status", false);
-            logger.error("内部参数转化异常:"+e.getMessage());
             response.setStatus(HttpStatus.BAD_REQUEST.value());
+            return ResponseUtils.responseFail(e.getMessage(), this);
         } catch (Exception e) {
-            result.clear();
-            result.put("status", false);
-            logger.error(e.getMessage(), e);
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return ResponseUtils.responseFail(e.getMessage(), this);
         }
-        return result;
     }
 }
