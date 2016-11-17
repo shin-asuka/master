24aa22c82b91770b16d982fd77bdbb07679ce02d
zouqinghua <zouqinghua@vipkid.com.cn>
2016-11-17 20:25:40 +0800

    增加注解

--- trpm-web/src/main/java/com/vipkid/rest/web/TeacherLocationController.java
+++ trpm-web/src/main/java/com/vipkid/rest/web/TeacherLocationController.java
@@ -2,11 +2,12 @@ package com.vipkid.rest.web;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.base.Preconditions;
@@ -26,6 +27,7 @@ public class TeacherLocationController {
 
 	private Logger logger = LoggerFactory.getLogger(TeacherLocationController.class);
 	
+	@Resource
 	private TeacherAppService teacherAppService;
 
 	/**
