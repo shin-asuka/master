45a30e3da0c20a3c9239a9f9eee8729c5b4874fe
liuguowen <liuguowen@bogon.com>
2016-11-22 18:48:36 +0800

    读写分离修改

--- trpm-web/src/main/java/com/vipkid/dataSource/TypedReadWriteDataSourceRouter.java
+++ trpm-web/src/main/java/com/vipkid/dataSource/TypedReadWriteDataSourceRouter.java
@@ -1,20 +1,21 @@
 package com.vipkid.dataSource;
 
 import com.vipkid.dataSource.annotation.Master;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.lang.annotation.Annotation;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Created by liuguanqing on 16/5/10.
- * 基于@Master，@Slave注释类型的读写分离
- * 只有注释的接口才会读写分离，其他的默认仍然是master
+ * Created by liuguanqing on 16/5/10. 基于@Master，@Slave注释类型的读写分离 只有注释的接口才会读写分离，其他的默认仍然是master
  */
 public class TypedReadWriteDataSourceRouter extends AbstractRoutingDataSource {
 
-    private Integer slaves;//slaves的个数
+    private static Logger logger = LoggerFactory.getLogger(TypedReadWriteDataSourceRouter.class);
+
+    private Integer slaves;// slaves的个数
 
     public void setSlaves(Integer slaves) {
         this.slaves = slaves;
@@ -22,18 +23,12 @@ public class TypedReadWriteDataSourceRouter extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
-        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
         Annotation annotation = DataSourceHolder.get();
-        //默认还是master；等测试成熟以后，可以全量RW分离
-        if(annotation == null || !isReadOnly) {
+        if (annotation == null || annotation.annotationType() == Master.class) {
             return "WRITE";
         }
-
-        if(annotation.annotationType() == Master.class) {
-            return "WRITE";
-        }
-
         logger.info("TypedReadWriteDataSourceRouter set datasource [salve]");
+        
         ThreadLocalRandom random = ThreadLocalRandom.current();
         return "READ_" + random.nextInt(slaves);
     }
