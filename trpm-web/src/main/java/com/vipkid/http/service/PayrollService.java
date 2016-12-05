fd866a5bb04d1f63e670a698ba7986c68a92d040
Deng Peng <dengpeng@vipkid.com.cn>
2016-12-05 20:33:42 +0800

    page

--- trpm-web/src/main/java/com/vipkid/http/service/PayrollService.java
+++ trpm-web/src/main/java/com/vipkid/http/service/PayrollService.java
@@ -2,6 +2,9 @@ package com.vipkid.http.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,19 +45,34 @@ public class PayrollService extends HttpBaseService {
 		return jsonObject;
 	}
 
-	public FindPayrollItemByTypeWithPageResponse findPayrollItemByTypeWithPage(
-			 int slalaryTypeCourseAdditionRule, int teacherId, int month) {
+	public String findPayrollItemByTypeWithPage(int slalaryTypeCourseAdditionRule, int teacherId, int month,
+			Page page) {
+		Map<String, String> params = Maps.newHashMap();
 		
-		Page<Teacher> page = new Page<Teacher>(request, response);
+            params.put("id", new Integer(teacher.getId()).toString());
+            params.put("pageNo", pageNo.toString());
+            params.put("pageNo", "1");
+        if (pageSize != null) {
+            params.put("pageSize", pageSize.toString());
+        } else {
+            params.put("pageSize", "15");
+        }
 		
+
+		String url = new StringBuilder(super.serverAddress).append("/api/app/teacher/findPayrollItemByTypeWithPage")
+				.toString();
+		JSONObject jsonObject = null;
 		
-		String url = new StringBuilder(super.serverAddress).append("/api/app/teacher/findRuleByTeacherMonth").toString();
-		JSONObject jsonObject = null ;
-		 Map<String, String> params = Maps.newHashMap();
-	     params.put("teacherId", new Integer(teacherId).toString());
-	     params.put("month", new Integer(month).toString());
-		// TODO Auto-generated method stub
-		return null;
+		params.put("teacherId", new Integer(teacherId).toString());
+		params.put("month", new Integer(month).toString());
+		params.put("page", page.toString());
+		HttpResult result = WebUtils.post(url, params);
+		 String resultString = null;
+		if (HttpResult.STATUS_SUCCESS.equals(result.getStatus())) {
+            // jsonObject = JSONObject.parseObject(result.getResponse().toString());
+            resultString = result.getResponse().toString();
+        }
+        return resultString;
 	}
 	
 	private JSONObject getResult(Map<String, String> params, String url, JSONObject jsonObject) {
---
+++ trpm-web/src/main/java/com/vipkid/payroll/model/PayrollPage.java
@@ -0,0 +1,16 @@
+package com.vipkid.payroll.model;
+@SuppressWarnings("rawtypes")
+public class PayrollPage extends Page {
+	private static final long serialVersionUID = 1L;
+
+	private int allTotalSalary;
+
+	public int getAllTotalSalary() {
+		return allTotalSalary;
+	}
+
+	public void setAllTotalSalary(int allTotalSalary) {
+		this.allTotalSalary = allTotalSalary;
+	}
+
+}
