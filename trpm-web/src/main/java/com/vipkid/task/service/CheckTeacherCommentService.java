78f2b9f9adc2e29e72dd39c30258deee104ad926
tianwenqi <tianwenqi@vipkid.com.cn>
2016-12-07 15:26:30 +0800

    teacherComment表迁移至homework库--代码merge错误-revert

--- trpm-web/src/main/java/com/vipkid/task/service/CheckTeacherCommentService.java
+++ trpm-web/src/main/java/com/vipkid/task/service/CheckTeacherCommentService.java
@@ -46,8 +46,8 @@ public class CheckTeacherCommentService {
 	public void remindTeacherComment(int hours){
 
 		//查询出hours个小时以前已经AS_SCHEDULED的课程
-		Date startDate = UADateUtils.getDateOclockByBeforeHours(hours);
-		Date endDate = UADateUtils.getDateOclockByBeforeHours(hours-1);
+		Date startDate = UADateUtils.getDateByBeforeHours(hours+1);
+		Date endDate = UADateUtils.getDateByBeforeHours(hours);
 
 		String startTime = UADateUtils.format(startDate, UADateUtils.defaultFormat) ;
 		String endTime = UADateUtils.format(endDate, UADateUtils.defaultFormat) ;
