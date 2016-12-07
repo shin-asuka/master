4d3cfb697d323455613317a384c3fd8099213176
ALong <zengweilong@vipkid.com.cn>
2016-12-07 20:45:04 +0800

    submited cacen bug fix

--- trpm-web/src/main/java/com/vipkid/recruitment/interview/service/InterviewService.java
+++ trpm-web/src/main/java/com/vipkid/recruitment/interview/service/InterviewService.java
@@ -17,12 +17,14 @@ import com.vipkid.enums.OnlineClassEnum;
 import com.vipkid.enums.TeacherApplicationEnum.Result;
 import com.vipkid.enums.TeacherApplicationEnum.Status;
 import com.vipkid.enums.TeacherEnum.LifeCycle;
+import com.vipkid.enums.TeacherLockLogEnum.Reason;
 import com.vipkid.recruitment.common.service.RecruitmentService;
 import com.vipkid.recruitment.dao.InterviewDao;
 import com.vipkid.recruitment.dao.TeacherApplicationDao;
 import com.vipkid.recruitment.dao.TeacherApplicationLogDao;
 import com.vipkid.recruitment.dao.TeacherLockLogDao;
 import com.vipkid.recruitment.entity.TeacherApplication;
+import com.vipkid.recruitment.entity.TeacherLockLog;
 import com.vipkid.recruitment.interview.InterviewConstant;
 import com.vipkid.recruitment.utils.ReturnMapUtils;
 import com.vipkid.trpm.dao.LessonDao;
@@ -154,7 +156,7 @@ public class InterviewService {
         }
 
         //book的课程在开课前1小时之内不允许book
-        if(System.currentTimeMillis() + InterviewConstant.BOOK_TIME > onlineClass.getScheduledDateTime().getTime()){
+        if((System.currentTimeMillis() + InterviewConstant.BOOK_TIME) > onlineClass.getScheduledDateTime().getTime()){
             return ReturnMapUtils.returnFail("Class is about to start is not allowed to book !");
         }
         //约课老师必须是INTERVIEW的待约课老师
@@ -193,37 +195,50 @@ public class InterviewService {
      */
     public Map<String,Object> cancelInterviewClass(long onlineClassId,Teacher teacher){
         OnlineClass onlineClass = this.onlineClassDao.findById(onlineClassId);
-        //课程没有找到，无法book
+
+        //课程没有找到，无法取消
         if(onlineClass == null){
-            return ReturnMapUtils.returnFail("The online class not exis:"+onlineClassId);
+            return ReturnMapUtils.returnFail("The online class doesn't exist:"+onlineClassId);
         }
-        //onlineClassId 必须是OPEN 课
-        if(!OnlineClassEnum.ClassStatus.OPEN.toString().equalsIgnoreCase(onlineClass.getStatus())){
-            return ReturnMapUtils.returnFail("This class("+onlineClassId+") is empty or anyone else has been booked !");
+
+        //class already start, can't cancel error
+        if(System.currentTimeMillis() > onlineClass.getScheduledDateTime().getTime()){
+            return ReturnMapUtils.returnFail("The class already start, can't cancel error:"+onlineClassId);
         }
-        //book的课程在开课前1小时之内不允许book
-        if(System.currentTimeMillis() + InterviewConstant.BOOK_TIME > onlineClass.getScheduledDateTime().getTime()){
-            return ReturnMapUtils.returnFail("Class is about to start is not allowed to book !");
-        }
-        //约课老师必须是INTERVIEW的待约课老师
-        List<TeacherApplication> listEntity = teacherApplicationDao.findCurrentApplication(teacher.getId());
-        if(CollectionUtils.isNotEmpty(listEntity)){
+
+        List<TeacherApplication> listEntity = this.teacherApplicationDao.findCurrentApplication(teacher.getId());
+        //如果步骤中无数据则不允许cancel
+        if(CollectionUtils.isEmpty(listEntity)){
+            return ReturnMapUtils.returnFail("You do not have permission to cancel this course:"+onlineClassId);
+        }else{
             TeacherApplication teacherApplication = listEntity.get(0);
-            //存在步骤，但步骤中已经存在待审核的课程 不允许继续book
-            if(teacherApplication.getOnlineClassId() != 0 && StringUtils.isBlank(teacherApplication.getResult())){
-                return ReturnMapUtils.returnFail("You have booked a class already. Please refresh your page !"+onlineClassId);
+            //如果步骤中有数据并且数据不是本次cancel的课程 则不允许cancel
+            if(teacherApplication.getOnlineClassId() != onlineClass.getId()){
+                return ReturnMapUtils.returnFail("You have already cancelled this class. Please refresh your page !");
+            }else{
+                //果步骤中有数据并且数据不是本次cancel的课程 但管理端已经审核，不允许cancel
+                if(StringUtils.isNotBlank(teacherApplication.getResult())){
+                    return ReturnMapUtils.returnFail("This class already audited. Please refresh your page !");
+                }
             }
         }
-        //判断剩余可取消次数
-        if(recruitmentService.getRemainRescheduleTimes(teacher, Status.INTERVIEW.toString(), Result.CANCEL.toString()) <= 0){
-            return ReturnMapUtils.returnFail("You cancel too many times, can't book the class !");
+
+        //保存cancel记录
+        this.teacherApplicationLogDao.saveCancel(teacher.getId(), listEntity.get(0).getId(), Status.INTERVIEW, Result.CANCEL, onlineClass);
+
+        int count = recruitmentService.getRemainRescheduleTimes(teacher, Status.INTERVIEW.toString(), Result.CANCEL.toString());
+
+        if(count == 0){
+            userDao.doLock(teacher.getId());
+            teacherLockLogDao.save(new TeacherLockLog(teacher.getId(), Reason.RESCHEDULE.toString(), LifeCycle.INTERVIEW.toString()));
         }
-        //执行BOOK逻辑
-        String dateTime = DateFormatUtils.format(onlineClass.getScheduledDateTime(),"yyyy-MM-dd HH:mm:ss");
-        Map<String,Object> result = OnlineClassProxy.doBookRecruitment(teacher.getId(), onlineClass.getId(), ClassType.TEACHER_RECRUITMENT,dateTime);
+
+        //执行Cancel逻辑
+        Map<String,Object> result = OnlineClassProxy.doCancelRecruitement(teacher.getId(), onlineClass.getId(), ClassType.TEACHER_RECRUITMENT);
+        result.put("count", count);
         if(ReturnMapUtils.isFail(result)){
             //一旦失败，抛出异常回滚
-            throw new RuntimeException("The a book class result is fail!"+result.get("info"));
+            throw new RuntimeException("The a cancel class result is fail ! "+result.get("info"));
         }
         return result;
     }
