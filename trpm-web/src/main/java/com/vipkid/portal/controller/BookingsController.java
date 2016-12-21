16bbc6cb8be8be0b8a49add109de27841bb9baa3
liuguowen <liuguowen@bogon.com>
2016-12-21 15:17:49 +0800

    增加test case

--- trpm-web/src/main/java/com/vipkid/portal/controller/BookingsController.java
+++ trpm-web/src/main/java/com/vipkid/portal/controller/BookingsController.java
@@ -1,15 +1,22 @@
 package com.vipkid.portal.controller;
 
+import com.google.api.client.repackaged.com.google.common.base.Preconditions;
+import com.google.api.client.util.Maps;
+import com.vipkid.file.utils.StringUtils;
+import com.vipkid.http.utils.JsonUtils;
+import com.vipkid.portal.entity.ScheduledRequest;
 import com.vipkid.portal.service.BookingsService;
 import com.vipkid.rest.config.RestfulConfig;
 import com.vipkid.trpm.entity.Teacher;
 import com.vipkid.trpm.service.rest.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
@@ -26,10 +33,25 @@ public class BookingsController {
     private LoginService loginService;
 
     @RequestMapping(value = "/scheduled", method = RequestMethod.GET, produces = RestfulConfig.JSON_UTF_8)
-    public Map<String, Object> scheduled(@RequestParam String type,
-                    @RequestParam(required = false, defaultValue = "0") int weekOffset) {
-        Teacher teacher = loginService.getTeacher();
-        return bookingsService.doSchedule(weekOffset, teacher.getId(), teacher.getTimezone(), type);
+    public Map<String, Object> scheduled(@RequestBody String body, HttpServletResponse response) {
+        try {
+            ScheduledRequest scheduledRequest = JsonUtils.toBean(body, ScheduledRequest.class);
+            Preconditions.checkArgument(StringUtils.isNotBlank(scheduledRequest.getType()));
+
+            Teacher teacher = loginService.getTeacher();
+            return bookingsService.doSchedule(scheduledRequest.getWeekOffset(), teacher.getId(), teacher.getTimezone(),
+                            scheduledRequest.getType());
+        } catch (IllegalArgumentException e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            Map<String, Object> resultMap = Maps.newHashMap();
+            resultMap.put("error", "Wrong params format");
+            return resultMap;
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            Map<String, Object> resultMap = Maps.newHashMap();
+            resultMap.put("error", "Server error");
+            return resultMap;
+        }
     }
 
 }
---
+++ trpm-web/src/test/java/com/vipkid/portal/controller/BookingsControllerTest.java
@@ -0,0 +1,77 @@
+package com.vipkid.portal.controller;
+
+import com.vipkid.trpm.proxy.RedisProxy;
+import com.vipkid.trpm.util.CacheUtils;
+import org.community.tools.JsonTools;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.vipkid.http.utils.JsonUtils;
+import com.vipkid.portal.entity.ScheduledRequest;
+import com.vipkid.trpm.entity.User;
+import com.vipkid.trpm.service.rest.LoginService;
+
+/**
+ * Created by liuguowen on 2016/12/21.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring/applicationContext.xml"})
+@WebAppConfiguration
+@Transactional
+public class BookingsControllerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookingsControllerTest.class);
+
+    private static final String TOKEN = "295b9605-e1e2-4101-8946-aa2bb9e3c658";
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private LoginService loginService;
+
+    @Autowired
+    private RedisProxy redisProxy;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        String key = CacheUtils.getUserTokenKey(TOKEN);
+        User user = loginService.getLoginUser("lilibo1@vipkid.com.cn");
+        redisProxy.set(key, JsonTools.getJson(user), 12 * 60 * 60);
+    }
+
+    @Test
+    public void testScheduled() throws Exception {
+        ScheduledRequest scheduledRequest = new ScheduledRequest();
+        scheduledRequest.setType("MAJOR");
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/portal/scheduled").header("Authorization", TOKEN)
+                        .content(JsonUtils.toJSONString(scheduledRequest));
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        if (HttpStatus.OK.value() == mvcResult.getResponse().getStatus()) {
+            logger.info("Result Json {}", mvcResult.getResponse().getContentAsString());
+        } else {
+            logger.info("Result Status {}", mvcResult.getResponse().getStatus());
+        }
+    }
+
+}
