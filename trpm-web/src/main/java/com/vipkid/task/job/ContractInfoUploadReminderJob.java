197a950701d0491f1f6b44962cdef43213d1394c
xingxuelin <xingxuelin@vipkid.com.cn>
2016-12-21 21:27:57 +0800

    TR_Phase2 v6.1 - FINAL

--- trpm-web/src/main/java/com/vipkid/task/job/ContractInfoUploadReminderJob.java
+++ trpm-web/src/main/java/com/vipkid/task/job/ContractInfoUploadReminderJob.java
@@ -97,7 +97,7 @@ public class ContractInfoUploadReminderJob {
 
 		if (auditTime.after(startTime) && auditTime.before(endTime)){
 			//userDao.doLock(teacher.getId());
-			//teacherLockLogDao.save(new TeacherLockLog(teacher.getId(), TeacherLockLogEnum.Reason.CONTRACT_INFO_NO_UPLOAD.toString(), TeacherEnum.LifeCycle.CONTRACT_INFO.toString()));
+			//teacherLockLogDao.save(new TeacherLockLog(teacher.getId(), TeacherLockLogEnum.Reason.CONTRACT_INFO_UPLOAD.toString(), TeacherEnum.LifeCycle.CONTRACT_INFO.toString()));
 			logger.info("【JOB.EMAIL.ContractInfoUploadReminderJob】LOCK: Cost {}ms. teacherId = {}, teacherEmail = {}", stopwatch.elapsed(TimeUnit.MILLISECONDS), teacher.getId(), teacher.getEmail());
 		} else {
 			String email = teacher.getEmail();
