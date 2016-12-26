6b6385ab90ec177629f648a16fff706e894145fa
liuguowen <liuguowen@bogon.com>
2016-12-26 19:30:25 +0800

    增加参数beijingTime

--- trpm-web/src/main/java/com/vipkid/portal/entity/ZoneTime.java
+++ trpm-web/src/main/java/com/vipkid/portal/entity/ZoneTime.java
@@ -18,13 +18,17 @@ public class ZoneTime {
     /* 格式化的本地时区时间 */
     private String localTime;
 
+    /* 格式化的用于显示的北京时间 */
+    private String beijingTime;
+
     private Map<String, Object> onlineClassMap;
 
     public ZoneTime(TimeSlot timeSlot, String timezone) {
         Instant instant = timeSlot.toDate().toInstant();
 
         LocalDateTime localDateTimeBeiJing = LocalDateTime.ofInstant(instant, SHANGHAI);
-        this.formatToBeiJing = localDateTimeBeiJing.format(FMT_YMD_HMA_US);
+        this.formatToBeiJing = localDateTimeBeiJing.format(FMT_YMD_HMS);
+        this.beijingTime = localDateTimeBeiJing.format(FMT_YMD_HMA_US);
         this.dateFromBeiJing = Date.from(localDateTimeBeiJing.atZone(SHANGHAI).toInstant());
 
         this.localTime = formatTo(instant, timezone, FMT_YMD_HMA_US);
@@ -40,6 +44,10 @@ public class ZoneTime {
 
     public String getLocalTime() {
         return localTime;
+    }
+
+    public String getBeijingTime() {
+        return beijingTime;
     }
 
     public Map<String, Object> getOnlineClassMap() {
