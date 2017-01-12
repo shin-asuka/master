835a7c1f30fa5f1e13266d2f76d2d3fc84198f4c
tianwenqi <tianwenqi@vipkid.com.cn>
2017-01-12 11:49:01 +0800

    测试是否支持app和pc的统一

---
+++ trpm-web/src/main/java/com/vipkid/trpm/service/huanxin/HuanxinService.java
@@ -0,0 +1,25 @@
+package com.vipkid.trpm.service.huanxin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 实现描述:
+ *
+ * @author steven
+ * @version v1.0.0
+ * @see
+ * @since 2017/1/11 下午3:33
+ */
+@Service
+public class HuanxinService {
+
+    private Logger logger = LoggerFactory.getLogger(HuanxinService.class);
+
+    private final static String URL = "https://a1.easemob.com/easemob-demo/chatdemoui/users";
+
+    private void signUp(String teacherId){
+
+    }
+}
