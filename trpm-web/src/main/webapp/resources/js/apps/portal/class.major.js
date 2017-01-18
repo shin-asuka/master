88fdfda40809f93fb8c790a4a97b0db3aa4d950b
niunglu <ninglu@vipkid.com.cn>
2017-01-18 16:22:40 +0800

    添加时间限制

--- trpm-web/src/main/webapp/resources/js/apps/portal/class.major.js
+++ trpm-web/src/main/webapp/resources/js/apps/portal/class.major.js
@@ -493,10 +493,18 @@ define(["function","jquery-bootstrap","jquery-load","countdown" ], function() {
 	}
 
 	var changeClassRoom = function(scheduledDateTime,serialNumber,oldStatus,is24Hour) {
+		var now = new Date();
+		now.setMinutes(now.getDate() - 30);
+		var scheduledTime = scheduledDateTime.replace(/-/g,"/");
+		var scheduledDate = new Date(Date.parse(scheduledTime));
+		var inTime = false;
+		if (now<scheduledDate){
+			inTime = true;
+		}
 		if(!scheduledDateTime || !serialNumber || !oldStatus){
 			return;
 		}
-		if (oldStatus == "FINISHED" && is24Hour){
+		if (oldStatus == "FINISHED" && is24Hour && inTime){
 			var url = webPath + "/changeClassroom.json";
 			var interval = 5 * 1000;
 			var params = {
