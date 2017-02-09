d18bec6bdcb3966bd68922f14195baf53b33315a
yangchao <yangchao@vipkid.com.cn>
2017-02-09 20:58:13 +0800

    add param

--- trpm-web/src/main/java/com/vipkid/rest/portal/vo/TranslationVo.java
+++ trpm-web/src/main/java/com/vipkid/rest/portal/vo/TranslationVo.java
@@ -7,6 +7,7 @@ public class TranslationVo {
 
     private Long id;
     private String text;
+    private String time;
 
     public Long getId() {
         return id;
@@ -22,5 +23,13 @@ public class TranslationVo {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 }
