009e9f4064de9a62338b5c875ecbaf236f9e1430
zengweilong <zengweilong@LP-0074.vipkid.work>
2017-02-13 17:23:02 +0800

    add Service 类

---
+++ trpm-web/src/main/java/com/vipkid/portal/classroom/service/ClassroomService.java
@@ -0,0 +1,12 @@
+package com.vipkid.portal.classroom.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassroomService {
+
+	private static Logger Logger = LoggerFactory.getLogger(ClassroomService.class);
+	
+}
---
+++ trpm-web/src/main/java/com/vipkid/portal/classroom/service/CommentsService.java
@@ -0,0 +1,11 @@
+package com.vipkid.portal.classroom.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentsService {
+
+	private static Logger Logger = LoggerFactory.getLogger(CommentsService.class);
+}
