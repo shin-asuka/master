cc4c78867049909d7967396f020f25dd5cf09941
mike.lee <mike.lee@qunar.com>
2017-02-16 10:42:58 +0800

    添加error log 邮件报警

---
+++ trpm-web/src/main/java/com/vipkid/common/log/AlertLogFilter.java
@@ -0,0 +1,35 @@
+package com.vipkid.common.log;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+
+/**
+ * Created by liyang on 2017/2/15.
+ */
+public class AlertLogFilter extends Filter<ILoggingEvent> {
+
+    @Override
+    public FilterReply decide(ILoggingEvent event) {
+
+        if(event.getLevel().levelInt < Level.WARN_INT){
+            return FilterReply.DENY;
+        }
+
+        boolean notifyOn = true;
+        if(!notifyOn){
+            return FilterReply.DENY;
+        }
+
+        if (event.getLoggerName().equals("org.apache.commons.httpclient.HttpMethodBase") &&
+                event.getMessage().contains("Using getResponseBodyAsStream instead is recommended.")) {
+            return FilterReply.DENY;
+        }
+
+
+
+        return FilterReply.ACCEPT;
+    }
+
+}
