fe1628ec852216e1567329152736a0f1cad58ce7
mike.lee <mike.lee@qunar.com>
2017-02-17 14:31:52 +0800

    根据环境来判断是否发报警邮件，目前发报警邮件的环境有 a,preonline,production

--- trpm-web/src/main/java/com/vipkid/common/log/AlertLogFilter.java
+++ trpm-web/src/main/java/com/vipkid/common/log/AlertLogFilter.java
@@ -4,14 +4,31 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
+import com.google.api.client.util.Lists;
+import org.community.config.PropertyConfigurer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by liyang on 2017/2/15.
  */
 public class AlertLogFilter extends Filter<ILoggingEvent> {
 
+    private static final List<String> nonFilterEnvNames = new ArrayList<>(Arrays.asList("a", "preonline", "production"));
+
+
+
+
     @Override
     public FilterReply decide(ILoggingEvent event) {
+
+        String envName = PropertyConfigurer.stringValue("environment.name");
+
+        if(!nonFilterEnvNames.contains(envName)){
+            return FilterReply.DENY;
+        }
 
         if(event.getLevel().levelInt < Level.ERROR_INT){
             return FilterReply.DENY;
