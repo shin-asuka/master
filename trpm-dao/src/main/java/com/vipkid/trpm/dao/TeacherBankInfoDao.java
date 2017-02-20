5cfcad25fec0a3249da73fe30d65e1a868995e9c
liuguowen <liuguowen@bogon.com>
2017-02-20 15:07:40 +0800

    修改bankinfo逻辑

--- trpm-dao/src/main/java/com/vipkid/trpm/dao/TeacherBankInfoDao.java
+++ trpm-dao/src/main/java/com/vipkid/trpm/dao/TeacherBankInfoDao.java
@@ -1,5 +1,6 @@
 package com.vipkid.trpm.dao;
 
+import com.google.common.base.Preconditions;
 import com.vipkid.trpm.entity.TeacherBankInfo;
 import org.community.dao.support.MapperDaoTemplate;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -12,6 +13,30 @@ public class TeacherBankInfoDao extends MapperDaoTemplate<TeacherBankInfo> {
     @Autowired
     public TeacherBankInfoDao(SqlSessionTemplate sqlSessionTemplate) {
         super(sqlSessionTemplate, TeacherBankInfo.class);
+    }
+
+    public TeacherBankInfo getTeacherBankInfo(long teacherId) {
+        Preconditions.checkArgument(0 != teacherId);
+        TeacherBankInfo teacherBankInfo = new TeacherBankInfo();
+        teacherBankInfo.setTeacherId(teacherId);
+        return selectOne(teacherBankInfo);
+    }
+
+    public void updateTeacherBankInfo(long teacherId, TeacherBankInfo teacherBankInfo) {
+        Preconditions.checkArgument(0 != teacherId);
+        Preconditions.checkNotNull(teacherBankInfo);
+
+        TeacherBankInfo oldBankInfo = new TeacherBankInfo();
+        oldBankInfo.setTeacherId(teacherId);
+        oldBankInfo = getTeacherBankInfo(teacherId);
+
+        if (null != oldBankInfo) {
+            teacherBankInfo.setId(oldBankInfo.getId());
+            update(teacherBankInfo);
+        } else {
+            teacherBankInfo.setTeacherId(teacherId);
+            save(teacherBankInfo);
+        }
     }
 
 }
