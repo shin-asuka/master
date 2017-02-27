3a4e67ae445d5ca30a0df3ea4c1b83f2b604b6bc
mike.lee <mike.lee@qunar.com>
2017-02-27 12:30:12 +0800

    去掉无用的代码

--- trpm-web/src/main/java/com/vipkid/http/utils/JsonUtils.java
+++ trpm-web/src/main/java/com/vipkid/http/utils/JsonUtils.java
@@ -11,10 +11,6 @@ import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
