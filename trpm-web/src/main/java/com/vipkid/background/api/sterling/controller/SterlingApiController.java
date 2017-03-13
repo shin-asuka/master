0d60203d707ac63e40208792eca16284ff8bfdeb
mike.lee <mike.lee@qunar.com>
2017-03-13 18:45:14 +0800

    sterling 接口屏蔽拦截

--- trpm-web/src/main/java/com/vipkid/background/api/sterling/controller/SterlingApiController.java
+++ trpm-web/src/main/java/com/vipkid/background/api/sterling/controller/SterlingApiController.java
@@ -1,7 +1,13 @@
 package com.vipkid.background.api.sterling.controller;
 
+import com.amazonaws.util.json.Jackson;
 import com.google.common.collect.Maps;
 import com.vipkid.background.api.sterling.dto.CandidateInputDto;
+import com.vipkid.background.api.sterling.service.SterlingApiUtils;
+import com.vipkid.http.utils.JacksonUtils;
+import com.vipkid.rest.utils.ApiResponseUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,44 +20,47 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class SterlingApiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(SterlingApiController.class);
 
 
     @RequestMapping("/createCandidates.json")
     public Object createCandidates(CandidateInputDto candidateInputDto){
+        logger.info(JacksonUtils.toJSONString(candidateInputDto));
 
-
-        return Maps.newHashMap();
+        return ApiResponseUtils.buildSuccessDataResp("success");
     }
 
 
 
     @RequestMapping("/updateCandidates.json")
     public Object updateCandidates(CandidateInputDto candidateInputDto){
+        logger.info(JacksonUtils.toJSONString(candidateInputDto));
 
-
-        return Maps.newHashMap();
+        return ApiResponseUtils.buildSuccessDataResp("success");
     }
 
 
     @RequestMapping("/createScreening.json")
     public Object createScreening(Long teacherId){
-
-        return Maps.newHashMap();
+        logger.info("teacher:{}",teacherId);
+        return ApiResponseUtils.buildSuccessDataResp("success");
     }
 
 
     @RequestMapping("/createPreAdverse.json")
     public Object createPreAdverse(Long teacherId){
-
-        return Maps.newHashMap();
+        logger.info("teacher:{}",teacherId);
+        return ApiResponseUtils.buildSuccessDataResp("success");
     }
 
 
 
     @RequestMapping("/callback.json")
     public Object  callback(HttpServletRequest request){
-
-        return Maps.newHashMap();
+        String type = request.getParameter("type");
+        String payload = request.getParameter("payload");
+        logger.info("type:{},payload:{}",type,payload);
+        return ApiResponseUtils.buildSuccessDataResp("success");
     }
 
 
