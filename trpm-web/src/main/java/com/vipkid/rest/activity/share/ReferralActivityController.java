e12135f48790b9afa76ca45452f19a68aace4d62
zengweilong <zengweilong@LP-0074.vipkid.work>
2017-03-24 16:14:06 +0800

    page json referral

--- trpm-web/src/main/java/com/vipkid/rest/activity/share/ReferralActivityController.java
+++ trpm-web/src/main/java/com/vipkid/rest/activity/share/ReferralActivityController.java
@@ -15,15 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.google.common.collect.Maps;
+import com.vipkid.rest.RestfulController;
 import com.vipkid.rest.activity.dto.ShareHandleDto;
 import com.vipkid.rest.activity.dto.StartHandleDto;
 import com.vipkid.rest.activity.dto.SubmitHandleDto;
 import com.vipkid.rest.config.RestfulConfig;
+import com.vipkid.rest.interceptor.annotation.RestInterface;
 import com.vipkid.rest.utils.ApiResponseUtils;
 
 @Controller
 @RequestMapping("/portal/referral/activity")
-public class ReferralActivityController {
+public class ReferralActivityController extends RestfulController{
 
 	
 	private final static Logger logger = LoggerFactory.getLogger(ReferralActivityController.class);
@@ -67,8 +69,8 @@ public class ReferralActivityController {
 	 */
 	@RequestMapping(value = "/click", method = RequestMethod.GET, produces = RestfulConfig.JSON_UTF_8)
 	public Map<String, Object> clickHandle(HttpServletRequest request, HttpServletResponse response,
-			@RequestParam(value="linkSourceId",required=false,defaultValue="2")Integer linkSourceId, 
-			@RequestParam(value="shareRecordId",required=false,defaultValue="2")Integer shareRecordId){		
+			@RequestParam(value="linkSourceId",required=true,defaultValue="2")Integer linkSourceId, 
+			@RequestParam(value="shareRecordId",required=false)Integer shareRecordId){		
 		try{
 			Map<String,Object> result = Maps.newHashMap();
 			
@@ -88,6 +90,9 @@ public class ReferralActivityController {
 	
 	/**
 	 * 开始答题事件记录及数据获取接口
+	 * 1.shareRecordId
+	 * 2.candidateKey (从考必须有，非从靠可以不需要有)
+	 * 获取考试JSON串，新的考试ID
 	 * @param request
 	 * @param response
 	 * @param shareRecordId
@@ -112,7 +117,7 @@ public class ReferralActivityController {
 	}
 	
 	/**
-	 * 答题结果提交接口
+	 * 完成答题结果提交接口
 	 * @param request
 	 * @param response
 	 * @param bean
@@ -120,6 +125,33 @@ public class ReferralActivityController {
 	 */
 	@RequestMapping(value = "/submit", method = RequestMethod.POST, produces = RestfulConfig.JSON_UTF_8)
 	public Map<String, Object> submitHandle(HttpServletRequest request, HttpServletResponse response,@RequestBody SubmitHandleDto bean){		
+		try{
+			Map<String,Object> result = Maps.newHashMap();
+			
+			
+			return ApiResponseUtils.buildSuccessDataResp(result);
+        } catch (IllegalArgumentException e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+			logger.error(e.getMessage(),e);
+			return ApiResponseUtils.buildErrorResp(-6, "参数类型转化错误:"+e.getMessage());
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			logger.error(e.getMessage(),e);
+			return ApiResponseUtils.buildErrorResp(-7, "服务器异常:"+e.getMessage());
+        }
+	}
+	
+	
+	/**
+	 * 完成答题结果提交接口(需要登陆后才能访问该接口)
+	 * @param request
+	 * @param response
+	 * @param bean
+	 * @return
+	 */
+	@RestInterface
+	@RequestMapping(value = "/checkResult", method = RequestMethod.POST, produces = RestfulConfig.JSON_UTF_8)
+	public Map<String, Object> checkResult(HttpServletRequest request, HttpServletResponse response){		
 		try{
 			Map<String,Object> result = Maps.newHashMap();
 			
