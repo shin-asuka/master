32ef038c76fe33ca91e05ef230eaa9d9d42b7e4e
rentingji <rentingji@vipkid.com.cn>
2017-03-27 19:36:39 +0800

    邮件模板更改

--- trpm-web/src/main/java/com/vipkid/recruitment/practicum/service/PracticumService.java
+++ trpm-web/src/main/java/com/vipkid/recruitment/practicum/service/PracticumService.java
@@ -163,7 +163,7 @@ public class PracticumService {
         String logpix = "onlineclassId:"+onlineClassId+";teacherId:"+teacher.getId();
         
         if(recruitmentService.teacherIsApplicationFinished(teacher)){
-            return ReturnMapUtils.returnFail("Your recruitment process is over already, Please refresh your page !","PRACTICUM:"+teacher.getId());
+            return ReturnMapUtils.returnFail("Your recruitment process is over already, Please refresh your page !","PRACTICUM: "+teacher.getId());
         }
                 
         OnlineClass onlineClass = this.onlineClassDao.findById(onlineClassId);
