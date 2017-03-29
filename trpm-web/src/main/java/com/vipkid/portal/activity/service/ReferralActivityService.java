a9ae83a098bbe9832532745478896d33ffe5844f
zengweilong <zengweilong@LP-0074.vipkid.work>
2017-03-29 18:15:21 +0800

    判断等于 相反

--- trpm-web/src/main/java/com/vipkid/portal/activity/service/ReferralActivityService.java
+++ trpm-web/src/main/java/com/vipkid/portal/activity/service/ReferralActivityService.java
@@ -102,7 +102,7 @@ public class ReferralActivityService {
 		if(NumericUtils.isNull(activityExam)){
 			return ReturnMapUtils.returnFail(-2, "没有找到考试记录,非法的分享");
 		}
-		if(StringUtils.equals(candidateKey, activityExam.getCandidateKey())){
+		if(!StringUtils.equals(candidateKey, activityExam.getCandidateKey())){
 			return ReturnMapUtils.returnFail(-3, "非法提交，测试ID "+activityExamId+" 和 candidateKey "+candidateKey+"不匹配");
 		}
 		if(NumericUtils.isNull(activityExam.getShareRecordId())){
