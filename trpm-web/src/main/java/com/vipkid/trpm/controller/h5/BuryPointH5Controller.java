06c2fc27ecfa0a48c123edc5c7994ea85937be6a
yangchao <yangchao@vipkid.com.cn>
2017-03-30 12:08:40 +0800

    add url

--- trpm-web/src/main/java/com/vipkid/trpm/controller/h5/BuryPointH5Controller.java
+++ trpm-web/src/main/java/com/vipkid/trpm/controller/h5/BuryPointH5Controller.java
@@ -4,6 +4,7 @@ import com.vipkid.rest.utils.BuryPointUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/h5/burypoint")
 public class BuryPointH5Controller {
+
+    @RequestMapping(value = "shareParentFeedback", method = RequestMethod.GET)
     public static Boolean shareParentFeedBack(Integer teacherId,Long onlineClassId,Long feedbackId){
         BuryPointUtils.shareParentFeedBack(teacherId,onlineClassId,feedbackId,"H5");
         return true;
