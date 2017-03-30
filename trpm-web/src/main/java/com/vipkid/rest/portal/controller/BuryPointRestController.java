e8fe8fc73e397e227a1c8802e1d490ab5f86e2f4
yangchao <yangchao@vipkid.com.cn>
2017-03-30 16:27:23 +0800

    repair

--- trpm-web/src/main/java/com/vipkid/rest/portal/controller/BuryPointRestController.java
+++ trpm-web/src/main/java/com/vipkid/rest/portal/controller/BuryPointRestController.java
@@ -8,11 +8,12 @@ import com.vipkid.rest.utils.BuryPointUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by LP-813 on 2017/3/30.
  */
-@Controller
+@RestController
 @RestInterface(lifeCycle = TeacherEnum.LifeCycle.REGULAR)
 @RequestMapping("/burypoint/")
 public class BuryPointRestController {
