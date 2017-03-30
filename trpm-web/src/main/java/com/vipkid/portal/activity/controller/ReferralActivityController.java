05dc1463113dcafa5f2f7b5803287fbfc91dd4f3
zengweilong <zengweilong@LP-0074.vipkid.work>
2017-03-30 17:40:42 +0800

    Rest

--- trpm-web/src/main/java/com/vipkid/portal/activity/controller/ReferralActivityController.java
+++ trpm-web/src/main/java/com/vipkid/portal/activity/controller/ReferralActivityController.java
@@ -11,10 +11,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Maps;
 import com.vipkid.enums.ShareActivityExamEnum.StatusEnum;
@@ -38,7 +38,7 @@ import com.vipkid.trpm.entity.ShareActivityExam;
 import com.vipkid.trpm.entity.ShareExamDetail;
 import com.vipkid.trpm.entity.User;
 
-@Controller
+@RestController
 @RequestMapping("/portal/referral/activity")
 public class ReferralActivityController extends RestfulController{
 
