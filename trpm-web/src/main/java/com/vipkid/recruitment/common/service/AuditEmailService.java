9c20b26974530de94debf879b0ce880204b41af7
liuguowen <liuguowen@bogon.com>
2017-04-06 14:38:52 +0800

    Format code

--- trpm-web/src/main/java/com/vipkid/recruitment/common/service/AuditEmailService.java
+++ trpm-web/src/main/java/com/vipkid/recruitment/common/service/AuditEmailService.java
@@ -65,7 +65,6 @@ public class AuditEmailService {
     @Autowired
     private TeacherApplicationDao teacherApplicationDao;
 
-
     @Autowired
     private TeacherPeCommentsDao teacherPeCommentsDao;
 
