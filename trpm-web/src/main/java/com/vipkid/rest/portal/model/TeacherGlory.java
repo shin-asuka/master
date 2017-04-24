ae61be96bd5e5a81fd95a6611ee6ad8fdd6d18e2
yangchao <yangchao@vipkid.com.cn>
2017-04-24 16:30:10 +0800

    glory interface

---
+++ trpm-web/src/main/java/com/vipkid/rest/portal/model/TeacherGlory.java
@@ -0,0 +1,98 @@
+package com.vipkid.rest.portal.model;
+
+/**
+ * Created by LP-813 on 2017/4/24.
+ */
+public class TeacherGlory {
+
+    private Integer gloryId;
+    private String name;
+    private Integer userId;
+    private String finishTime;
+    private Integer priority;
+    private String avatar;
+    private String title;
+    private String description;
+    private String shareTitle;
+    private String shareDescription;
+
+    public Integer getGloryId() {
+        return gloryId;
+    }
+
+    public void setGloryId(Integer gloryId) {
+        this.gloryId = gloryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(String finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getShareTitle() {
+        return shareTitle;
+    }
+
+    public void setShareTitle(String shareTitle) {
+        this.shareTitle = shareTitle;
+    }
+
+    public String getShareDescription() {
+        return shareDescription;
+    }
+
+    public void setShareDescription(String shareDescription) {
+        this.shareDescription = shareDescription;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
